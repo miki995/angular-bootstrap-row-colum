524929e14b14c6ba87f2d086898d07029cad0551
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2018-08-04 22:24:03 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  arrayOfItems = [
+    {
+      title: 'red',
+      subTitle: 'f00',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'blue',
+      subTitle: 'aaas',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'toto',
+      subTitle: 'f00',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'red',
+      subTitle: 'toto',
+      description: 'toto lorem toto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil t soil t'
+    },
+    {
+      title: 'lorem',
+      subTitle: 'toto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil t',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'red',
+      subTitle: 'toto',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'lorem',
+      subTitle: 'soil',
+      description: 'toto lorem ispum toto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil t t'
+    },
+    {
+      title: 'red',
+      subTitle: 'toto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil t',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'lorem',
+      subTitle: 'soil',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'red',
+      subTitle: 'toto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil t',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'lorem',
+      subTitle: 'soil',
+      description: 'toto loremtoto lorem toto lorem ispum soil ttoto lo' +
+        'rem ispum soil ttoto toto lorem ispum soil ttoto lorem ispum soil' +
+        ' tlorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil ttoto lorem' +
+        ' ispum soil t soil ttoto lorem ispum soil t ispum soil t'
+    },
+    {
+      title: 'red',
+      subTitle: 'toto',
+      description: 'toto lorem ispum soil t'
+    },
+    {
+      title: 'lorem',
+      subTitle: 'soil',
+      description: 'toto toto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil ttoto lorem ispum soil t ispum soil t'
+    }
+  ];
+  newArray = [];
+
+  ngOnInit() {
+
+    for (let i = 0; i < this.arrayOfItems.length; i += 3) {
+
+      this.newArray.push({ items: this.arrayOfItems.slice(i, i + 3) });
+    }
+  }
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Item } from '../model';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+  @Input() item: Item;
+}
